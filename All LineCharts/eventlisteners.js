b7453d8ea12f54091219730897ad9365a06a27bc
patli1998 <pratikpurohit1998@gmail.com>
2023-04-23 16:41:15 -0400

    added event listeners for line charts

--- All LineCharts/eventlisteners.js
+++ All LineCharts/eventlisteners.js
@@ -1,11 +1,19 @@
-var seaLevelBtn = document.getElementById('seaLevelBtn');
-seaLevelBtn.addEventListener('click', function() {
-  var seaLevelPlot = document.getElementById('seaLevelPlot');
-  seaLevelPlot.style.display = 'block';
-  var temperaturePlot = document.getElementById('temperaturePlot');
-  temperaturePlot.style.display = 'none';
-  var precipitationPlot = document.getElementById('precipitationPlot');
-  precipitationPlot.style.display = 'none';
-  var co2EmissionPlot = document.getElementById('co2EmissionPlot');
-  co2EmissionPlot.style.display = 'none';
-});
+function openChart(evt, chartName) {
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="chart" and hide them
+  tabcontent = document.getElementsByClassName("chart");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(chartName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
